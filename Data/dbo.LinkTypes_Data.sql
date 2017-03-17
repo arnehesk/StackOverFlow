432373ec5153266554bbd4d87587adc68e215a58
arnehesk <arneh.eskandari@red-gate.com>
2017-03-17 13:54:42 -0700

    Baseline commit

---
+++ Data/dbo.LinkTypes_Data.sql
@@ -0,0 +1,2 @@
+INSERT INTO [dbo].[LinkTypes] ([Id], [Type]) VALUES (1, 'Linked')
+INSERT INTO [dbo].[LinkTypes] ([Id], [Type]) VALUES (3, 'Duplicate')
---
+++ Data/dbo.PostTypes_Data.sql
@@ -0,0 +1,8 @@
+INSERT INTO [dbo].[PostTypes] ([Id], [Type]) VALUES (1, N'Question')
+INSERT INTO [dbo].[PostTypes] ([Id], [Type]) VALUES (2, N'Answer')
+INSERT INTO [dbo].[PostTypes] ([Id], [Type]) VALUES (3, N'Wiki')
+INSERT INTO [dbo].[PostTypes] ([Id], [Type]) VALUES (4, N'TagWikiExerpt')
+INSERT INTO [dbo].[PostTypes] ([Id], [Type]) VALUES (5, N'TagWiki')
+INSERT INTO [dbo].[PostTypes] ([Id], [Type]) VALUES (6, N'ModeratorNomination')
+INSERT INTO [dbo].[PostTypes] ([Id], [Type]) VALUES (7, N'WikiPlaceholder')
+INSERT INTO [dbo].[PostTypes] ([Id], [Type]) VALUES (8, N'PrivilegeWiki')
